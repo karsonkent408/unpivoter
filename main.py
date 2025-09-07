ed468093e60bc42643e448c7ec404ddd5de0c294
Karson Kent <karsonkent@Karsons-MacBook-Air.local>
2025-09-06 20:56:28 -0400

    added help tips

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import numpy as np
 import streamlit as st
 
 
+
 def main():
-    st.title('Unpivot your dumb table')
+    st.set_page_config(
+        page_title="Unpivot your dumb table",
+        page_icon="🤡",
+        layout="centered",
+    )
+    with st.container(horizontal_alignment="center", vertical_alignment="center"):
+        st.title('Unpivot your dumb table')
+        st.write("Why do clients refuse to give us data tables in a format we can use?")
+        st.write("No one will ever know, and that will never change.")
+        st.write("So here is an app to unpivot your dumb table into a long format")
+        st.write("It will also aggregate the data by the id vars and sum the value vars")
+        st.divider()
 
     uploaded_file = st.file_uploader("Upload your dumb table")
     
@@ -22,11 +34,21 @@ def main():
 
     headers = df.columns.tolist()
 
-    id_vars = st.multiselect("Select the id vars", headers, default=[])
+    id_vars = st.multiselect(
+        "Select the id vars", 
+        headers, 
+        default=[],
+        help="Select headers that will be used to identify the rows"
+    )
 
     value_candidates = [header for header in headers if header not in id_vars]
 
-    value_vars = st.multiselect("Select the value vars", value_candidates, default=[])
+    value_vars = st.multiselect(
+        "Select the value vars", 
+        value_candidates, 
+        default=[],
+        help="Select the headers that will be used to aggregate the data"
+    )
 
     value_name = st.text_input("Select the name of the value column", "Value")
 
@@ -36,9 +58,7 @@ def main():
         st.error("Please select the id vars, value vars, value name, and var name")
         return
 
-
     df_agg = df.groupby(id_vars, as_index=False)[value_vars].sum()
-
 
     new_df = df_agg.melt(id_vars=id_vars, value_vars=value_vars, value_name=value_name, var_name=var_name)
 
